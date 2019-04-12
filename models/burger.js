fff6a4fa25c83fb914b4a3897613d7f02c162412
bbierman <bbierman7@gmail.com>
2019-04-12 14:23:49 -0500

    added JawsDB to heroku app

--- models/burger.js
+++ models/burger.js
@@ -14,7 +14,7 @@ var burger = {
     });
   },
   update: function(objColVals, condition, cb) {
-    console.log("here in update: " + objColVals + ":" + condition);
+    console.log("here in update: " + objColVals + ":" + condition); 
     orm.update("burgers", objColVals, condition, function(res) {
       cb(res);
     });
